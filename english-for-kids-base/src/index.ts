5b783593b4ddcb8ab9b94603e6daabdba0798977
Sergei Demchuk <vinci.ru@mail.ru>
2021-06-26 20:14:31 +0300

    feat:add game field with cards

--- english-for-kids-base/src/index.ts
+++ english-for-kids-base/src/index.ts
@@ -7,7 +7,11 @@ burger?.addEventListener('click', () => {
   console.log('show');
   document.querySelector('.menu')?.classList.toggle('show');
 });
-const game = new Game();
+// const game = new Game();
 // el?.addEventListener('change', () => {
 //   console.log((el as any).prop('checked', true));
 // });
+
+// function gameFieldMode() {
+
+// }
